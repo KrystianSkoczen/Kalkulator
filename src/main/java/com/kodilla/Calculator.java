216bf90fc45a62e6175fdc9635a0cfc949c2a164
KrystianSkoczen <k.skoczen@wp.pl>
2018-04-03 11:50:03 +0200

    First commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,25 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Calculator {
+
+        double a = 5.2;
+        double b = 1.6;
+        public double addition() {
+            return a + b;
+        }
+        public double subtraction(){
+            return a - b;
+        }
+
+
+        public static void main(String[] args){
+            Calculator calculator = new Calculator();
+            double subtraction = calculator.subtraction();
+
+            System.out.println(subtraction);
+        }
+}
+
